98593e35651ffe124edd56f2cd458e46fc6d3449
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 09:28:32 +0200

    Agregar espaciado y estructura de contenedor para el componente SVG en App

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import SVG from './SVG/SVG';
 
 function App() {
   return (
-    <div className="flex items-start justify-center w-ful h-screen bg-[#515151]">
-      <SVG.KarmeleFill />
+    <div className="flex items-start justify-center p-[100px] w-ful h-screen bg-[#515151]">
+      <div className="flex flex-col items-center justify-start w-full h-full">
+        <SVG.KarmeleFill />
+      </div>
     </div>
   );
 }
