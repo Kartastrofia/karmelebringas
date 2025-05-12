e615de97b656de295685e997ef9231bd80435ae4
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 10:10:02 +0200

    Ajustar espaciado y alineación en el componente App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import SVG from './SVG/SVG';
 
 function App() {
   return (
-    <div className="flex items-start justify-center p-[100px] w-ful h-screen bg-[#515151]">
-      <div className="flex flex-col items-center justify-start w-full h-full">
+    <div className="flex items-start justify-center py-[100px] w-ful h-screen bg-[#515151]">
+      <div className="flex flex-col items-start justify-center p-3 w-full h-full bg-[#FFFFFF59]">
         <SVG.KarmeleFill />
       </div>
     </div>
