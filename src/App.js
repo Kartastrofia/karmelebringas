96514794a7c73ab161b13c2558f61751685789c0
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 10:13:59 +0200

    Ajustar espaciado en el componente App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SVG from './SVG/SVG';
 
 function App() {
   return (
-    <div className="flex items-start justify-center py-[100px] w-ful h-screen bg-[#515151]">
+    <div className="flex items-start justify-center py-[120px] w-ful h-screen bg-[#515151]">
       <div className="flex flex-col items-start justify-center p-3 w-full h-full bg-[#FFFFFF59]">
         <SVG.KarmeleFill />
       </div>
