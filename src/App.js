0ac20a7fe92c8561188623780c167854203f898b
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 11:25:15 +0200

    Refactor estructura del componente App y simplificar el componente Header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import SVG from './SVG/SVG';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-start py-[120px] w-ful h-screen bg-[#515151]">
-      <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
-        <SVG.KarmeleFill />
+    <div className="flex flex-col items-center justify-start py-[120px] w-ful min-h-screen">
+      {/* Parte superior pagina */}
+      <div className='flex flex-col items-center justify-start w-full grow'>
+        {/* Header */}
+        <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#92CDFF]">
+          <SVG.KarmeleFill />
+        </div>
+        {/* Main Content */}
+        <div className='flex items-center justify-center grow w-full bg-gradient-to-b from-[#069FD6] to-[#5EFADB]'>
+          <SVG.KarmeleFill width='302' height='95' color='#FFFFFF'/>
+        </div>
       </div>
-      <div className='flex items-center justify-center grow w-full'>
-        <SVG.KarmeleFill width='302' height='95' color='#FFFFFF'/>
-      </div>
-      <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
-        
-      </div>
+      {/* Footer */}
+      <footer className="flex flex-row items-center justify-center px-[32px] py-[16px] w-full bg-[#FBFFE5]">
+      </footer>
     </div>
   );
 }
--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,16 +1,10 @@
 import React from "react";
+import SVG from "../SVG/SVG";
 
 const Header = ({}) => {
   return (
-    <header>
-      <h1>My Application</h1>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+    <header className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
+      <SVG.KarmeleFill />
     </header>
   );
 }
