848eea843289c4fe575b7780ce4516a37cfb593f
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 10:24:17 +0200

    Agregar contenedor adicional para el componente SVG en App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
         <SVG.KarmeleFill />
       </div>
+      <div className='flex items-center justify-center grow w-full'>
+        <SVG.KarmeleFill width='302' height='95' color='#FFFFFF'/>
+      </div>
+      <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
+        
+      </div>
     </div>
   );
 }
