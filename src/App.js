956e9a88362398644325333a0ec4365115fd0a4f
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 12:42:17 +0200

    Agregar funcionalidad de menú desplegable en el componente Header y pasar propiedades para el control del menú móvil

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import SVG from './SVG/SVG';
 import Header from './Componentes/Header';
 
 function App() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-start w-ful min-h-screen">
       {/* Parte superior pagina */}
       <div className='flex flex-col items-center justify-start py-[120px] w-full grow bg-gradient-to-b from-[#069FD6] to-[#5EFADB]'>
         {/* Header */}
-        <Header/>
+        <Header mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen}/>
         {/* Main Content */}
         <div className='flex items-center justify-center grow w-full'>
           <SVG.KarmeleFill width='302' height='95' className='shadow-[0px_4px_4px_0px_#3A59D1]'/>
--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import SVG from "../SVG/SVG";
+import Dropdown from "../Componentes/Dropdown";
 
-const Header = ({}) => {
+const Header = ({mobileMenuOpen, setMobileMenuOpen}) => {
   return (
-    <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#92CDFF]">
+    <div className="flex flex-row items-center justify-between px-[32px] py-[16px] w-full bg-[#92CDFF]">
+      {/* IZQ */}
       <SVG.KarmeleFill/>
+      {/* DER */}
+      <div className="flex flex-col items-start gap-[40px] justify-center">
+        <Dropdown/>
+      </div>
     </div>
   );
 }
