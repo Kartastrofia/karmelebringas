a84d284a37198aab35321f7fb961dda271f435b0
Kartastrofia <karmele.bringas@gmail.com>
2025-05-12 10:26:11 +0200

    Ajustar la estructura del contenedor principal en el componente App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SVG from './SVG/SVG';
 
 function App() {
   return (
-    <div className="flex items-start justify-center py-[120px] w-ful h-screen bg-[#515151]">
+    <div className="flex flex-col items-center justify-start py-[120px] w-ful h-screen bg-[#515151]">
       <div className="flex flex-row items-center justify-start px-[32px] py-[16px] w-full bg-[#FFFFFF59]">
         <SVG.KarmeleFill />
       </div>
